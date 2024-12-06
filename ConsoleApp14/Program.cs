0d2c34a104de781e1051a6a57981cbb2c459174d
Solehzoda <salikhovajillo@gmail.com>
2024-12-06 19:11:01 +0500

    первый коммит

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0.0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                sum += 1.0 / i;
+            }
+            Console.WriteLine(sum);
+            }
+        }
+    }
